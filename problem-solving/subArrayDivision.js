3c8fe1371ed96fea308b2954e33e4b7ccfed3239
seraphyne <renatafd717@gmail.com>
2020-10-22 14:58:08 -0500

    sub-array birthday solution

---
+++ problem-solving/subArrayDivision.js
@@ -0,0 +1,13 @@
+function birthday(s, d, m) {
+  let count = 0;
+  let sum = 0;
+  for (let i = 0; i < m; i++) sum += s[i];
+  if (sum === d) count++;
+  for (let i = m, j = 0; i < s.length; i++) {
+    sum -= s[j];
+    sum += s[i];
+    j++;
+    if (sum === d) count++;
+  }
+  return count;
+}
