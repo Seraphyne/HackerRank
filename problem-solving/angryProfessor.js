6631cbbebe07760f7d92b35c171a65fa4201595f
seraphyne <renatafd717@gmail.com>
2020-10-29 21:08:52 -0500

    angry professor

---
+++ problem-solving/angryProfessor.js
@@ -0,0 +1,9 @@
+function angryProfessor(k, a) {  
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] <= 0) {
+      count += 1;
+    }
+  }
+  return count >= k ? 'NO' : 'YES';
+}
