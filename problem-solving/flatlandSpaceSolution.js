2b55bbdb5cc72be0fbbbfb42e90f6ec55dfe4520
Seraphyne <renatafd717@gmail.com>
2020-11-15 03:48:52 -0600

    flatlan space solutions solution

---
+++ problem-solving/flatlandSpaceSolution.js
@@ -0,0 +1,11 @@
+function flatlandSpaceStations(n, c) {
+  let distances = [];
+  c.sort((a,b)=>a-b);
+  for (let i = 0; i < c.length-1; i++) {
+    const dist = Math.floor(Math.abs(c[i + 1] - c[i]) / 2);
+    distances.push(dist);
+  }
+  const before = Math.abs(c[0] - 0);
+  const after = Math.abs(n - c[c.length - 1])-1;
+  return Math.max(...distances,before,after);
+}
