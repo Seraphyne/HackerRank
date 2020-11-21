54740f9db52474d5452c76b06a74528abed2c817
Seraphyne <renatafd717@gmail.com>
2020-11-20 22:33:15 -0600

    added the infinite loop to the same file

--- ruby/helloHackerRank.rb
+++ ruby/helloHackerRank.rb
@@ -6,3 +6,11 @@ print self
 
 # Ruby Tutorial - Object Method Parameters
 a.range?(b, c)
+
+# Ruby Control Structures - Infinite Loop
+loop { coder.oh_one? ? break : coder.practice}
+# OR
+loop do
+    coder.practice
+    break if coder.oh_one?
+end
