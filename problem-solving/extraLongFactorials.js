cfd63fbcd1787f795993aeee08b05daf8ff0a5b1
RD <renatafd717@gmail.com>
2020-12-16 21:41:07 -0600

    extra long factorials solution

---
+++ problem-solving/extraLongFactorials.js
@@ -0,0 +1,10 @@
+function extraLongFactorials(n) {
+  let bigInt = BigInt(n);
+  let factorial = 1n;
+
+  for (let i = 0n; i < bigInt; i++) {
+    factorial *= bigInt - i;
+  }
+
+  console.log(factorial.toString());
+}
