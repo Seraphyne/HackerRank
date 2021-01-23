ff9f67aa73c65eb5808d2773dc5750d366c6c43c
RD <renatafd717@gmail.com>
2021-01-22 18:35:06 -0600

    3d surfice area solution

---
+++ problem-solving/3DSurficeArea.js
@@ -0,0 +1,21 @@
+function surfaceArea(A) {
+  let S = 0;
+  let kf = 0;
+  for (let i = 0; i < A.length; i++) {
+    for (let j = 0; j < A[i].length; j++) {
+      kf =
+        (j > 0 && A[i][j - 1]
+          ? A[i][j] > A[i][j - 1]
+            ? A[i][j - 1]
+            : A[i][j]
+          : 0) +
+        (i > 0 && A[i - 1][j]
+          ? A[i][j] > A[i - 1][j]
+            ? A[i - 1][j]
+            : A[i][j]
+          : 0);
+      S += 4 * A[i][j] + 2 - 2 * kf;
+    }
+  }
+  return S;
+}
